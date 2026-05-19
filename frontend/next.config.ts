14171b82c5ef09d7afa25004391f485841300762
Mayur Upadhyay <mayur123a@gmail.com>
2026-05-19 17:22:25 +0530

    chore: updates next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = { poll: 1000, aggregateTimeout: 300 };
+    return config;
+  },
+  allowedDevOrigins: ["192.168.101.11"],
 };
 
 export default nextConfig;
